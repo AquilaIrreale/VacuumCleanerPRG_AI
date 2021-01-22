71ef21feebc2d5a104f9373e196c8ed6a50e2f9d
m1gnus <m1gnus@protonmail.com>
2021-01-22 16:04:57 +0100

    Fix read_board

--- vision.py
+++ vision.py
@@ -1,58 +1,66 @@
 import cv2
-from imutils import contours
 import numpy as np
 
 
-def read_board(path: str):
+CONTOUR_MAX_SIZE = 1000
+CONTOUR_MAX_SIZE_2 = 50000
+
+
+def read_board(file):
     # Load image, grayscale, and adaptive threshold
-    image = cv2.imread(path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,57,5)
-    
+    image = cv2.imread(file)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
+
     # Filter out all numbers and noise to isolate only boxes
-    cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        area = cv2.contourArea(c)
-        if area < 1000:
-            cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+    cnts, _ = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(
+            thresh_image,
+            [c for c in cnts if cv2.contourArea(c) < CONTOUR_MAX_SIZE],
+            -1, (0, 0, 0), -1)
 
     # Fix horizontal and vertical lines
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
-    
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
-    
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 5))
+    thresh_image = cv2.morphologyEx(thresh_image, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
+
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 1))
+    thresh_image = cv2.morphologyEx(thresh_image, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4)
+
     # Sort by top to bottom and each row by left to right
-    invert = 255 - thresh
-        
-    cnts = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    invert = 255 - thresh_image
+
+    cnts, _ = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # https://answers.opencv.org/question/179510/how-can-i-sort-the-contours-from-left-to-right-and-top-to-bottom/
     # TODO: improve
-    cnts = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[0]**(3/2) + cv2.boundingRect(ctr)[1] * image.shape[0])
+    def contours_key(c):
+        x, y, w, h = cv2.boundingRect(c)
+        imw, imh, depth = image.shape
+        return x**(3/2) + y*imw
 
-    rooms_rows = []
-    row = []
-    for (_, c) in enumerate(cnts, 1):
-        area = cv2.contourArea(c)
-        if area < 50000: # controllo necessario perche senno verranno presi anche i quadrati rappresentanti l'intera griglia e i sotto quadrati
-            (cnts, _) = contours.sort_contours(list([c]), method="left-to-right")
-            rooms_rows.append(cnts)
+    cnts = np.array([c for c in sorted(cnts, key=contours_key)
+                        if cv2.contourArea(c) < CONTOUR_MAX_SIZE_2])
 
-    rooms_imgs = []
+    rightmost_x = 0
+    for i, c in enumerate(cnts):
+        x, y, w, h = cv2.boundingRect(c)
+        if x < rightmost_x:
+            break
+        rightmost_x = x
+    ncols = i
 
     # Find bounding box and extract ROI
-    for row in rooms_rows:
-        for c in row:
-            x,y,w,h = cv2.boundingRect(c)
-            ROI = image[y:y+h, x:x+w]
-            ROI = cv2.resize(ROI, (28, 28))
-            rooms_imgs.append(ROI)
-    
-    return rooms_imgs
+    def trim_by_contour(c):
+        x, y, w, h = cv2.boundingRect(c)
+        return image[y:y+h, x:x+w]
+
+    cnts = np.array([trim_by_contour(c) for c in cnts])
+    return cnts.reshape((len(cnts)//i, i, *cnts.shape[1:]))
 
 
 if __name__ == "__main__":
-    print(read_board('images/digital.jpg'))
+    board = read_board('images/digital.jpg')
+    print(board.shape)
+    for row in board:
+        for image in row:
+            cv2.imshow("Test image", image)
+            cv2.waitKey(0)
