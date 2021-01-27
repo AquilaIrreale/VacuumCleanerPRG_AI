0506cc70cf5bbcea131ab092bd933f74eef240ec
Simone Cimarelli <aquilairreale@ymail.com>
2021-01-27 17:50:57 +0100

    Implement state_distance() and moves()

--- game.py
+++ game.py
@@ -45,20 +45,55 @@ def parse_board(classes_file, board):
     return board_layout, dirt, start_pos, final_pos
 
 
-def solve():
+def manhattan_distance(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return abs(x2-x1) + abs(y2-y1)
+
+
+def state_distance(state1, state2):
+    return (int(state1.dirt.sum())
+            - int(state2.dirt.sum())
+            + manhattan_distance(state1.pos, state2.pos))
+
+
+def moves(board_layout, state):
+    h, w = board_layout.shape
+    x, y = state.pos
+
+    if state.dirt[y, x]:
+        new_dirt = state.dirt[:]
+        new_dirt[y, x] -= 1
+        yield "Clean", State((x, y), new_dirt)
+
+    moves = {
+        "Right": ( 1,  0),
+        "Down":  ( 0,  1),
+        "Left":  (-1,  0),
+        "Up":    ( 0, -1)
+    }
+    for move, (dx, dy) in moves.items():
+        new_x, new_y = x+dx, y+dy
+        if 0 <= new_x < w and 0 <= new_y < h and board_layout[new_y, new_x]:
+            yield move, State((new_x, new_y), state.dirt)
+
+
+def test():
     board = np.array([ # TODO: get from vision
         [0, 1, 2],
         [3, 4, 5],
         [0, 1, 4]])
 
-    board, dirt, start_pos, final_pos = parse_board("model/classes", board)
+    layout, dirt, start_pos, final_pos = parse_board("model/classes", board)
     start_state = State(start_pos, dirt)
     final_state = State(final_pos, dirt*0)
 
-    print(board)
+    print(layout)
     print(start_state)
     print(final_state)
 
+    print(list(moves(layout, start_state)))
+
 
 if __name__ == "__main__":
-    solve()
+    test()
